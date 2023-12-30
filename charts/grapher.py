7940a5e6a3bde67b708fb4754b58cced81eefb39
John Samarasinghe <john.samarasinghe@outlook.com>
2023-12-30 22:33:34 +0530

    Optimizations & Bug fixes

--- charts/grapher.py
+++ charts/grapher.py
@@ -5,6 +5,8 @@ def grapher_category_dictionary(graph_dict, x_column, cat_column, data_column, x
     colors = ['b', 'g', 'r', 'c', 'm', 'y']
     color_val = 0
 
+    bar_width = 0.25
+
     data = {}
     total_data = {}
 
@@ -29,14 +31,17 @@ def grapher_category_dictionary(graph_dict, x_column, cat_column, data_column, x
     fig1.set_figwidth(10)
     fig1.set_figheight(5)
 
+    prev_bar = None
+
     for cat in data.keys():
-        plt.bar(list(data[cat].keys()), list(data[cat].values()), color=colors[color_val], label=cat)
+        plt.bar(list(data[cat].keys()), list(data[cat].values()), color=colors[color_val], label=cat, bottom=prev_bar)
+        prev_bar = cat
 
         color_val = color_val + 1
         if color_val == len(colors):
             color_val = 0
 
-    plt.plot(list(total_data.keys()), list(total_data.values()), color=colors[color_val], label="Total")
+    # plt.plot(list(total_data.keys()), list(total_data.values()), color=colors[color_val], label="Total")
 
     plt.title(chart_title)
     plt.xlabel(x_name)
