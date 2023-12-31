50a38fe6f5da02a50d1bf858b820aa5623c4ecf5
John Samarasinghe <john.samarasinghe@outlook.com>
2023-12-31 17:17:58 +0530

    Optimizations & Bug fixes

--- maintenance/backup.py
+++ maintenance/backup.py
@@ -21,22 +21,33 @@ class BackUp:
     def run_code(self):
         for file in self.copy_files:
             destination = os.path.join(self.backup_location, file)
+            if not os.path.exists(os.path.dirname(destination)):
+                os.makedirs(os.path.dirname(destination))
+
             shutil.copy(file, destination)
             logger.log(f"Copied {file} -> {destination}")
 
         for file in self.move_files:
             destination = os.path.join(self.backup_location, file)
+            if not os.path.exists(os.path.dirname(destination)):
+                os.makedirs(os.path.dirname(destination))
+
             shutil.move(file, destination)
             logger.log(f"Moved {file} -> {destination}")
 
         for folder in self.copy_folders:
             destination = os.path.join(self.backup_location, folder)
+            if not os.path.exists(destination):
+                os.makedirs(destination)
+
             shutil.copytree(folder, destination)
             logger.log(f"Copied {folder} -> {destination}")
 
         for folder in self.move_folders:
             allfiles = glob.glob(os.path.join(folder, '*_A_*'), recursive=True)
             destination = os.path.join(self.backup_location, folder)
+            if not os.path.exists(destination):
+                os.makedirs(destination)
 
             for file_path in allfiles:
                 dst_path = os.path.join(destination, os.path.basename(file_path))
