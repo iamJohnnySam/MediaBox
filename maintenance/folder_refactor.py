c529dd716dadfc579a98b14cd09cf455bf081bf1
John Samarasinghe <john.samarasinghe@outlook.com>
2024-01-04 19:32:00 +0530

    Optimizations & Bug fixes

--- maintenance/folder_refactor.py
+++ maintenance/folder_refactor.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import re
 import shutil
@@ -16,40 +17,69 @@ class RefactorFolder:
         files, directories = self.get_file_and_directory(self.path)
         self.sort_torrent_files(files, self.path)
 
+        last_loc = None
+
         for directory in directories:
             directory_path = os.path.join(self.path, directory)
             sub_files, sub_directories = self.get_file_and_directory(directory_path)
-            self.sort_torrent_files(sub_files, directory_path)
+            last_loc = self.sort_torrent_files(sub_files, directory_path)
+
+            if "Subs" in sub_directories:
+                self.move_subs_folder(directory_path, last_loc)
 
             sub_files, sub_directories = self.get_file_and_directory(directory_path)
             if len(sub_directories) == 0 and len(sub_files) == 0:
                 try:
-                    # os.rmdir(directory_path)
+                    os.rmdir(directory_path)
                     logger.log(f"Directory '{directory_path}' has been successfully deleted.", source=self.source)
                 except OSError as e:
                     logger.log(f"Error: {str(e)}", source=self.source)
             else:
                 logger.log(f"Cannot Delete '{directory_path}'. Not empty!", source=self.source, message_type="warn")
 
+    def move_subs_folder(self, directory_path, last_loc):
+        subs_folder = os.path.join(directory_path, "Subs")
+        allfiles = glob.glob(os.path.join(subs_folder, '*_A_*'), recursive=True)
+        destination = os.path.join(last_loc, "Subs")
+        if not os.path.exists(os.path.dirname(destination)):
+            os.makedirs(os.path.dirname(destination))
+
+        for file_path in allfiles:
+            dst_path = os.path.join(destination, os.path.basename(file_path))
+            shutil.move(file_path, dst_path)
+            logger.log(f"Moved {file_path} -> {dst_path}", source=self.source)
+
+        sub_files, sub_directories = self.get_file_and_directory(subs_folder)
+        if len(sub_directories) == 0 and len(sub_files) == 0:
+            try:
+                os.rmdir(subs_folder)
+                logger.log(f"Directory '{subs_folder}' has been successfully deleted.", source=self.source)
+            except OSError as e:
+                logger.log(f"Error: {str(e)}", source=self.source)
+        else:
+            logger.log(f"Cannot Delete '{subs_folder}'. Not empty!", source=self.source, message_type="warn")
+
     def sort_torrent_files(self, files, directory):
+        base_loc = None
         for file in files:
             file_name, tv_show, movie, subtitle, base_name = self.breakdown_torrent_file_name(file)
             logger.log(f'{file_name}, {tv_show}, {movie}, {subtitle}, {base_name}', source=self.source)
             if tv_show and not movie:
+                base_loc = os.path.join(global_var.torrent_tv_shows, base_name)
                 self.move_file(os.path.join(directory, file),
-                               os.path.join(global_var.torrent_tv_shows,
-                                            base_name),
+                               base_loc,
                                file_name)
             elif movie and not tv_show:
+                base_loc = os.path.join(global_var.torrent_movies, base_name)
                 self.move_file(os.path.join(directory, file),
-                               os.path.join(global_var.torrent_movies,
-                                            base_name),
+                               base_loc,
                                file_name)
             else:
+                base_loc = os.path.join(global_var.torrent_unknown,base_name)
                 self.move_file(os.path.join(directory, file),
-                               os.path.join(global_var.torrent_unknown,
-                                            base_name),
+                               base_loc,
                                base_name + file_name)
+        return base_loc
 
     def get_file_and_directory(self, path):
         files_list = []
@@ -85,7 +115,7 @@ class RefactorFolder:
         match_tv = re.search('S[0-9][0-9]E[0-9][0-9]', file_name, flags=re.IGNORECASE)
         match_quality = re.search('[0-9][0-9][0-9]p', file_name, flags=re.IGNORECASE)
 
-        if match_tv:
+        if match_tv and match_video:
             tv_show = True
             movie = False
             if "." in file_name:
