e7c963fa7e5cc7d9b4f64bf8f2171e225fe154e2
John Samarasinghe <john.samarasinghe@outlook.com>
2024-01-25 18:00:40 +0530

    Optimizations & Bug fixes

--- communication/communicator.py
+++ communication/communicator.py
@@ -676,11 +676,11 @@ class Communicator(CommunicatorBase):
 
         if not sql_databases["administration"].exists(self.database_groups,
                                                       f"group_name = 'news_{data[1]}' AND chat_id = '{from_id}'") == 0:
-            self.manage_chat_group('news_{data[1]}', from_id, add=False, remove=True)
+            self.manage_chat_group(f'news_{data[1]}', from_id, add=False, remove=True)
             reply_text = f"You are Unsubscribed from {data[1]}."
 
         else:
-            self.manage_chat_group('news_{data[1]}', from_id)
+            self.manage_chat_group(f'news_{data[1]}', from_id)
             reply_text = f"You are now Subscribed to {data[1]}."
 
         try:
