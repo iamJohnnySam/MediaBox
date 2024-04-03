91d817fd6c7150a2c4af59a9b44835246ea3c3bf
John Samarasinghe <john.samarasinghe@outlook.com>
2024-04-03 21:11:19 +0530

    Code Improvements and feature additions

--- modules/cctv_checker.py
+++ modules/cctv_checker.py
@@ -75,12 +75,12 @@ class CCTVChecker(Module):
                     if not os.path.exists(sav_cctv):
                         os.makedirs(sav_cctv)
 
-                    file_name = f"{datetime.now().strftime('%Y-%m-%d, %H-%M-%S')}-{date}-{val:.3f}.jpg"
+                    file_name = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}-{date}-{val:.3f}.jpg"
                     t = 1
                     while os.path.isfile(os.path.join(sav_cctv, file_name)):
                         file_name = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}-{date}-{val:.3f}({t}).jpg"
                         t = t+1
-                        file_name = file_name.replace(":", "").replace(" ", "")
+                    file_name = file_name.replace(":", "").replace(" ", "")
                     move_destination = shutil.move(att_path, os.path.join(sav_cctv, file_name))
                     log(self._job.job_id, f"Image Saved in {move_destination} with sus level at {val:.3f}")
 
