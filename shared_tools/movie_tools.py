c990dadd47219f896fae19d9bd487e03cf82428c
John Samarasinghe <john.samarasinghe@outlook.com>
2024-06-09 17:47:08 +0530

    Code Improvements and feature additions

--- shared_tools/movie_tools.py
+++ shared_tools/movie_tools.py
@@ -23,6 +23,6 @@ def get_movie_info(job_id: int, movie: str):
 
     for movie in movies:
         log(job_id=job_id, msg=f"Found Movie: {movie.title} ({movie.year})")
-        urllib.request.urlretrieve(movie.poster_path, f"{movie.title} ({movie.year}).jpg")
+        urllib.request.urlretrieve(movie.poster_url, f"{movie.title} ({movie.year}).jpg")
 
     return movies
