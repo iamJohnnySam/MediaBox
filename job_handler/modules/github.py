ef41ec54d2f2f213f56d20b137f4f6d9d1695b8a
John Samarasinghe <john.samarasinghe@outlook.com>
2024-06-09 15:16:00 +0530

    Code Improvements and feature additions

--- job_handler/modules/github.py
+++ job_handler/modules/github.py
@@ -1,6 +1,7 @@
 import git
 
 from job_handler.base_module import Module
+from shared_models import configuration
 from shared_models.job import Job
 from shared_models.message import Message
 from shared_tools.logger import log
@@ -9,7 +10,8 @@ from shared_tools.logger import log
 class GitHub(Module):
     def __init__(self, job: Job):
         super().__init__(job)
-        self.repo = git.Repo('/')
+        self.config = configuration.Configuration().admin
+        self.repo = git.Repo(self.config["location"])
 
     def pull_repo(self):
         current = self.repo.head.commit
