bbf58970763f499160d0d846c7bc802c1c34ab20
John Samarasinghe <john.samarasinghe@outlook.com>
2024-06-23 20:16:39 +0530

    Code Improvements and feature additions

--- shared_tools/movie_tools.py
+++ shared_tools/movie_tools.py
@@ -10,17 +10,7 @@ from shared_tools.logger import log
 def get_movie_info(job_id: int, movie: str = None) -> (list, list):
     tmdb = TMDb(key=passwords.tmdb_api)
 
-    movies1 = tmdb.search().movies(query=movie)
-
-    d = re.search(r'\d{4}', movie)
-    if d is not None:
-        movies2 = tmdb.search().movies(query=movie[0:d.start()].strip())
-        movies = list(set(movies1).union(set(movies2)))
-    else:
-        movies = movies1
-
-    if len(movies) == 0:
-        return [], []
+    movies = tmdb.search().movies(query=movie)
 
     movie_titles = []
     movie_posters = []
@@ -36,6 +26,22 @@ def get_movie_info(job_id: int, movie: str = None) -> (list, list):
 
         movie_titles.append(title)
         movie_posters.append(poster_path)
+
+    d = re.search(r'\d{4}', movie)
+    if d is not None:
+        movies2 = tmdb.search().movies(query=movie[0:d.start()].strip())
+        for movie in movies2:
+            if movie.poster_path is None or movie.title is None or movie.year is None:
+                continue
+            title = f"{movie.title} ({movie.year})".replace("/", "")
+            if title in movie_titles:
+                continue
+            log(job_id=job_id, msg=f"Found Movie: {title}")
+            poster_path = f"resources/movie_poster/{title}.jpg".replace(":", "")
+            urllib.request.urlretrieve(url="https://image.tmdb.org/t/p/w185" + movie.poster_path,
+                                       filename=poster_path)
+            movie_titles.append(title)
+            movie_posters.append(poster_path)
 
     return movie_titles, movie_posters
 
